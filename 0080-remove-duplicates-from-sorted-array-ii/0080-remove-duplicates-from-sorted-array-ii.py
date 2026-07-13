f70ee81477fe30acf7b47abfd374b25a3c5649bd
Shiban Siddiqui <101477637+Shibansiddiqui@users.noreply.github.com>
2026-07-13 22:03:36 +0530

    Time: 90 ms (40.91%), Space: 22.4 MB (24.81%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -4,6 +4,8 @@ class Solution:
 
         start = 1
         for i in range(2,len(nums)):
+            if  len(nums) < 2:
+                return nums
             if nums[start-1] != nums[i]:
                 start += 1
                 nums[start] = nums[i]
