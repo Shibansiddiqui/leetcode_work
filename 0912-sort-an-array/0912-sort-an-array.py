1f6b328b6c7123ff2d96b199c52f141718a98017
Shiban Siddiqui <101477637+Shibansiddiqui@users.noreply.github.com>
2026-07-22 02:45:29 +0530

    Time: 67 ms (82.27%), Space: 28.8 MB (21.96%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -7,13 +7,13 @@ class Solution:
         #             nums[j], nums[j+1] = nums[j+1], nums[j]
         maxi = max(nums)
         mn = min(nums)
-        freq_list = [0] * (maxi-mn+1)
+        freq_list = [0] * (maxi-mn+1) #creating correct storrange
         for i in nums:
-            freq_list[i - mn] += 1
+            freq_list[i - mn] += 1 # using mn so we can avoid using negative indexes
         nums = []
         for j in range(len(freq_list)):
             while freq_list[j] > 0:
-                nums.append(j + mn)
+                nums.append(j + mn) # converting back to orignal indexes, by subracting from mn that we added earlier
                 freq_list[j] -= 1
 
         return nums            
