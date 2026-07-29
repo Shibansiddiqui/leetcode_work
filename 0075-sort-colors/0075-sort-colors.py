24009179730f9a4a3f0fabda7cf095b9ec7ec1d2
Shiban Siddiqui <101477637+Shibansiddiqui@users.noreply.github.com>
2026-07-30 00:04:22 +0530

    Time: 0 ms (100%), Space: 19.2 MB (60.56%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,15 +3,31 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        maxn = max(nums)
-        freq = [0] * (maxn + 1) 
+        # maxn = max(nums)
+        # freq = [0] * (maxn + 1) 
        
-        for num in nums: 
-            freq[num] += 1
-        k = 0
-        print(freq)
-        for i in range(maxn+1):
-            while freq[i] > 0:
-                nums[k] = i
-                k += 1
-                freq[i] -= 1
+        # for num in nums: 
+        #     freq[num] += 1
+        # k = 0
+        # print(freq)
+        # for i in range(maxn+1):
+        #     while freq[i] > 0:
+        #         nums[k] = i
+        #         k += 1
+        #         freq[i] -= 1
+        left = 0
+        right = len(nums) -1
+        i = 0
+        while i <= right:
+            if nums[i] == 1:
+                i += 1
+            elif nums[i] == 0:
+                nums[i], nums[left] = nums[left], nums[i]
+                left += 1
+                i += 1
+            elif nums[i] == 2:
+                nums[i], nums[right] = nums[right], nums[i]
+                right -= 1
+
+
+
